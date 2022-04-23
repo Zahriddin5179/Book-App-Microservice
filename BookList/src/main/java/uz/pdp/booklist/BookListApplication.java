dcf2485830c9625b8068d2c1f460294bd190693c
Zahriddin5179 <zakhiriddinkhasanov013@gmail.com>
2022-04-23 11:57:07 +0500

    First commit

---
+++ BookList/src/main/java/uz/pdp/booklist/BookListApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.booklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookListApplication.class, args);
+    }
+
+}
